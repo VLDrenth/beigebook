b2c458db2a4d30612a11c7d52890f26137912892
vincentdrenth <laszlodrenth@gmail.com>
2025-01-25 11:16:05 +0000

    Expanded main function to include analysis

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Scraper and analyzer for the Federal Reserve's Beige book publications
 - pipeline: Contains module to take the scraped text, apply sentiment scoring, and produce a time-series parquet file
 - scraper: Contains the scraper module used to derive the raw text files
 - data_processing: Contains the methods used to clean and process the data. Too simple at the current stage to merit its own module, but will expand soon.
-- score_analyzer: Contains a wrapper for Language models to provide a sentiment score
+- sentiment: Contains a wrapper for Language models to provide a sentiment score
+- score_analyzer: Code used to analyze and produce plots for the scraped sentiment data
 - config: Contains some basic setup concerning paths and some parameters for running the programs
---
+++ main.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from src.config.config import Config
+from src.pipeline.processing import TextProcessor
+from src.scraper.scraper import BeigeBookScraper
+from src.score_analyzer.analyzer import ScoreAnalyzer
+
+def main(scrape: bool = False, sentiment: bool = False, analyze: bool = False):
+    """
+    Function that uses the modules to scrape the data and apply sentiment analysis.
+    """
+
+    if scrape:
+        BeigeBookScraper().fetch_all()
+        
+    if sentiment:
+        processor = TextProcessor()
+        print(processor.process_all(end_year=Config.END_YEAR))
+
+    if analyze:
+        analyzer = ScoreAnalyzer(filepath=Config.SENTIMENT_OUTPUT_DIR, output_dir=Config.PLOTS_OUTPUT_DIR)
+        analyzer.plot_regional_time_series()
+
+if __name__ == "__main__":
+    main(analyze=True)
